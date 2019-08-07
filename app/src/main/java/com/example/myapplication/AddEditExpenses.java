e660486d04dcd3826dcd0789957863f9e7797fea
Amoda <amoda29@gmail.com>
2019-08-07 11:30:02 +0530

    change Expenses Activity into fragment

--- app/src/main/java/com/example/myapplication/AddEditExpenses.java
+++ app/src/main/java/com/example/myapplication/AddEditExpenses.java
@@ -2,13 +2,32 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class AddEditExpenses extends AppCompatActivity {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
+public class AddEditExpenses extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private EditText select_date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_expenses);
+        getSupportFragmentManager().beginTransaction().replace( R.id.fragment_container , new AddExpense()).commit();
+
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
     }
 }
