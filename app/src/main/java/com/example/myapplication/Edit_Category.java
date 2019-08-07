ded72e4d02b2c5fb34d17c875c4428ba1501353f
vishvi97 <vishviwathasha@gmail.com>
2019-08-07 17:07:05 +0530

    Add edit category details page

--- app/src/main/java/com/example/myapplication/Edit_Category.java
+++ app/src/main/java/com/example/myapplication/Edit_Category.java
@@ -2,13 +2,35 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Edit_Category extends AppCompatActivity {
+
+    private EditText categoryName,categoryType, categoryDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit__category);
+        setContentView(R.layout.activity_edit_category);
+
+       Intent intent = getIntent();
+
+        categoryName = (EditText)findViewById(R.id.editText);
+        categoryDescription =(EditText)findViewById(R.id.edit_description);
+        //categoryType = (EditText)findViewById(R.id.income_btn_radio);
+
+        if(intent.getExtras()!=null){
+            Bundle data_bundle = intent.getExtras();
+            categoryName.setText( data_bundle.getString("category_name"));
+            categoryDescription.setText(data_bundle.getString("categoryDescription"));
+
+        }
+
     }
+
+
+
 }
--- app/src/main/java/com/example/myapplication/ViewCategoryDetails.java
+++ app/src/main/java/com/example/myapplication/ViewCategoryDetails.java
@@ -2,13 +2,47 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ViewCategoryDetails extends AppCompatActivity {
+
+    private ImageButton edit_categorybtn;
+    private TextView categoryName , description, category_type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_category_details);
+
+
+        edit_categorybtn = (ImageButton) findViewById(R.id.edit_btn);
+        edit_categorybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                edit_categorybtn= findViewById(R.id.edit_btn);
+                categoryName = findViewById(R.id.category_text);
+                description = findViewById(R.id.category_view_description);
+               category_type = findViewById(R.id.categoryType);
+                Intent intent = new Intent(ViewCategoryDetails.this , Edit_Category.class);
+
+                Bundle bundle = new Bundle();
+               bundle.putString("CategoryType",category_type.getText().toString());
+                bundle.putString("category_name",categoryName.getText().toString());
+               bundle.putString("categoryDescription",description.getText().toString());
+
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
+
 }
