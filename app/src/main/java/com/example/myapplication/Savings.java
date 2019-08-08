3acafc2b74922c7b4b34a2820bcbaf47a97b488b
Amoda <it18108514@my.sliit.lk>
2019-08-08 20:29:00 +0530

    Add Saving page added

--- app/src/main/java/com/example/myapplication/Savings.java
+++ app/src/main/java/com/example/myapplication/Savings.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -11,15 +12,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class Savings extends Fragment {
-
+    private FloatingActionButton plusBtn;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_savings, container, false);
 
+         plusBtn = view.findViewById(R.id.add_saving_btn);
+         plusBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent = new Intent( getActivity() , Add_Saving.class );
+                 startActivity(intent);
+             }
+         });
         return view;
     }
 
