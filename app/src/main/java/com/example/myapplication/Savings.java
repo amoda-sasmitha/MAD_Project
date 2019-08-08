ff9e4202be94f3e516c1912e970257dd1bdc7bbc
Amoda <amoda29@gmail.com>
2019-08-08 16:33:45 +0530

    Change the error of Savings fragment

---
+++ app/src/main/java/com/example/myapplication/Savings.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Savings extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_savings, container, false);
+
+        return view;
+    }
+
+
+}
