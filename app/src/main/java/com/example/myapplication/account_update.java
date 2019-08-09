7c446488f5c1d48a6d2bdb51a6e74ace7033d80d
padulapankaja <padulaguruge@gmail.com>
2019-08-09 09:14:10 +0530

    eeroes HAnndle

---
+++ app/src/main/java/com/example/myapplication/account_update.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link account_update.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link account_update#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class account_update extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_update, container, false);
+    }
+
+
+}
