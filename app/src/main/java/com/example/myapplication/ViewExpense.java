9b77471395b2701bd2cb7db0ee9fbbbcb2745dfb
Amoda <vishviwathasha@gmail.com>
2019-08-09 22:38:50 +0530

    Add delete Dialog and bug fixed

--- app/src/main/java/com/example/myapplication/ViewExpense.java
+++ app/src/main/java/com/example/myapplication/ViewExpense.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -21,12 +25,12 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class ViewExpense extends Fragment {
 
     private TextView data ,CategoryName , date , amount , description , account;
-    private ImageButton edit_btn;
+    private ImageButton edit_btn ,delete_btn;
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view_expense ,container , false);
-
+        delete_btn = view.findViewById(R.id.delete_btn);
         edit_btn = view.findViewById(R.id.edit_btn);
         CategoryName = view.findViewById(R.id.category_name);
         date = view.findViewById(R.id.date_text);
@@ -48,6 +52,36 @@ public class ViewExpense extends Fragment {
 
                 editExpense.setArguments(dataBundle);
                 getFragmentManager().beginTransaction().replace(R.id.fragment_container , editExpense  ).commit();
+            }
+        });
+
+
+        delete_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(getActivity() );
+                dialog.setContentView(R.layout.delete_message);
+                Button accept = dialog.findViewById(R.id.accept_btn);
+                TextView textView = dialog.findViewById(R.id.deleteText);
+                ImageButton close = dialog.findViewById(R.id.close_btn);
+
+                dialog.show();
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                accept.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getActivity() , MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
             }
         });
 
