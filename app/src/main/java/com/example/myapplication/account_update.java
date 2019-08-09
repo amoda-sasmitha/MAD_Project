f18cc2aed7ade23958317a5065dd68b8ef94cd29
Amoda <vishviwathasha@gmail.com>
2019-08-09 10:06:14 +0530

    correct some errors

--- app/src/main/java/com/example/myapplication/account_update.java
+++ app/src/main/java/com/example/myapplication/account_update.java
@@ -11,14 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link account_update.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link account_update#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class account_update extends Fragment {
 
     @Override
