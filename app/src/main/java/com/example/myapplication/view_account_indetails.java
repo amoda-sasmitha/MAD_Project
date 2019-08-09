230c740f6699de91c3cda69da5570ec50dff4bd4
Amoda <vishviwathasha@gmail.com>
2019-08-09 19:26:32 +0530

    Change some values

--- app/src/main/java/com/example/myapplication/view_account_indetails.java
+++ app/src/main/java/com/example/myapplication/view_account_indetails.java
@@ -2,13 +2,24 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class view_account_indetails extends AppCompatActivity {
-
+    private ImageView editBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_account_indetails);
+        editBtn = findViewById(R.id.edit_btn02);
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( view_account_indetails.this , EditAccount.class);
+                startActivity(intent);
+            }
+        });
     }
 }
