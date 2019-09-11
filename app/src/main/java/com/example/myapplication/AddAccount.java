73e275095e6ef41113972b3eb0a2c21e1eed18c5
padulapankaja <padulaguruge@gmail.com>
2019-09-12 00:05:21 +0530

    add_another Add account details view

--- app/src/main/java/com/example/myapplication/AddAccount.java
+++ app/src/main/java/com/example/myapplication/AddAccount.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class AddAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_account);
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerAccounts);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinnerAccounts, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
