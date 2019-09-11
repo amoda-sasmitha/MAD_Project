c826874592deb38e79f41a1c59168b23f1e74acf
padulapankaja <padulaguruge@gmail.com>
2019-09-12 01:16:02 +0530

    add_another Add account details view and do some db       parts

--- app/src/main/java/com/example/myapplication/AddAccount.java
+++ app/src/main/java/com/example/myapplication/AddAccount.java
@@ -1,13 +1,21 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import Database.DBhelper;
+import Models.AccountModel;
+
 public class AddAccount extends AppCompatActivity {
 
+    DBhelper db;
+    EditText accountName,  accountIniAmount, accountNumber, accountDescription;
+    Spinner accountType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,23 @@ public class AddAccount extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        accountName = (EditText) findViewById(R.id.textAddAcountName);
+        accountType = (Spinner) findViewById(R.id.spinnerAccounts);
+        accountIniAmount = (EditText) findViewById(R.id.add_account_initial_account_txt);
+        accountNumber = (EditText) findViewById(R.id.account_no_txt2);
+        accountDescription = (EditText) findViewById(R.id.account_des);
+
+    db = new DBhelper(this);
+
+
+    }
+    public void addAccountDetails(View view){
+        AccountModel am = new AccountModel();
+
+        am.setAccountName(accountName.getText().toString().trim());
+//        am.setAccountType(accountType.getV);
+
+
+
     }
 }
