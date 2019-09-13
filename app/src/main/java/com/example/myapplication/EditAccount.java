2c2b14c6cdf8c1300e8fcec19c007243a08b1cfa
padulapankaja <padulaguruge@gmail.com>
2019-09-13 20:27:43 +0530

    update edit account details

--- app/src/main/java/com/example/myapplication/EditAccount.java
+++ app/src/main/java/com/example/myapplication/EditAccount.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class EditAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_account  );
+        Spinner spinner = (Spinner) findViewById(R.id.editText3);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinnerAccounts, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
