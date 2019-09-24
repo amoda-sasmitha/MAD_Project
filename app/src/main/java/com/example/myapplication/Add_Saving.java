d96ab48244f27f71061313823af7198ad1a17b94
Pubudu Arosha <it18108514@my.sliit.lk>
2019-09-24 15:28:53 +0530

    Add saving validation

--- app/src/main/java/com/example/myapplication/Add_Saving.java
+++ app/src/main/java/com/example/myapplication/Add_Saving.java
@@ -8,17 +8,51 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Add_Saving extends AppCompatActivity {
+
+    Button save;
+    EditText sName, sDiscription, sTarget, sStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__saving);
 
-        Button save_btn = findViewById(R.id.save_btn);
+        sName = (EditText) findViewById(R.id.editText);
+        sDiscription = (EditText) findViewById(R.id.edit_description);
+        sTarget = (EditText) findViewById(R.id.addTargetAmountTypeText);
+        sStart = (EditText) findViewById(R.id.addStartAmountText);
+        save = findViewById(R.id.save_btn);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sName.length()== 0){
+                    sName.setError("Enter Saving Name");
+                }
+
+                if (sDiscription.length()== 0){
+                    sDiscription.setError("Enter Description");
+                }
+
+                if (sTarget.length()== 0){
+                    sTarget.setError("Enter Target Amount");
+                }
+
+                if (sStart.length()== 0){
+                    sStart.setError("Enter Start Amount");
+                }
+
+                else {
+                    Toast.makeText(Add_Saving.this,"Saving Successfully! :)",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
     }
 }
