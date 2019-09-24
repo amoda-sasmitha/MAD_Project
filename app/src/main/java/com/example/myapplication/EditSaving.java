381382cb890d0950363e9f57f9e9df42cd5b59b4
Pubudu Arosha <it18108514@my.sliit.lk>
2019-09-24 16:19:11 +0530

    Edit saving validation

--- app/src/main/java/com/example/myapplication/EditSaving.java
+++ app/src/main/java/com/example/myapplication/EditSaving.java
@@ -3,12 +3,46 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditSaving extends AppCompatActivity {
+
+    Button update;
+    EditText eSName, eSDescription, eSAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_saving);
-    }
+
+        eSName = (EditText) findViewById(R.id.editText1);
+        eSDescription = (EditText) findViewById(R.id.edit_description1);
+        eSAmount = (EditText) findViewById(R.id.amount_Text1);
+        update = (Button) findViewById(R.id.save_btn1);
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (eSName.length()== 0){
+                    eSName.setError("Enter Saving Name");
+                }
+
+                if (eSDescription.length()== 0){
+                    eSDescription.setError("Enter Description");
+                }
+
+                if (eSAmount.length()== 0){
+                    eSAmount.setError("Enter Target Amount");
+                }
+                else {
+                    Toast.makeText(EditSaving.this,"Update Successfully! :)",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+     }
 }
