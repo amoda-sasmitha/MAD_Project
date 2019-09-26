1b788bf7b51b999610d1c1143718bc5eba7e9c0b
padulapankaja <padulaguruge@gmail.com>
2019-09-26 21:14:56 +0530

    add validations to update Account

--- app/src/main/java/com/example/myapplication/EditAccount.java
+++ app/src/main/java/com/example/myapplication/EditAccount.java
@@ -65,42 +65,59 @@ public class EditAccount extends AppCompatActivity {
     submitBtn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            AccountModel newAccount = new AccountModel();
-            newAccount.setId( account.getId() );
-            newAccount.setAccountName( name.getText().toString().trim() );
-            newAccount.setAccountType( spinner.getItemAtPosition( spinner.getSelectedItemPosition()  ).toString().trim() );
-            newAccount.setAccountDescription( description.getText().toString().trim() );
 
-            boolean result = db.updateAccount( newAccount);
 
-            //inflate layout
-            View layout = getLayoutInflater().inflate( R.layout.toast_message , (ViewGroup) view.findViewById(R.id.toastRoot) );
-            TextView text = layout.findViewById(R.id.textMsg);
-            CardView background = layout.findViewById(R.id.back);
+            //validate user input
+            if (name.length() == 0) {
+                name.setError("Enter Account Name");
+            } else if (spinner.getSelectedItem().toString().equals("Select Account Type")) {
+                ((TextView) spinner.getSelectedView()).setError("Select Account Type");
+            } else if (amount.length() == 0) {
+                amount.setError("Enter Initial Amount");
+            } else if (accNumber.length() == 0) {
+                accNumber.setError("Enter Account NUmber");
+            } else if (description.length() == 0) {
+                description.setError("Enter Description");
+            } else {
 
-            //creat toast
-            Toast toast = new Toast( getApplicationContext() );
-            toast.setDuration(Toast.LENGTH_LONG);
-            toast.setGravity(Gravity.BOTTOM|Gravity.CENTER , 0 , 230 );
-    //set text for
-            if( result == false ){
-                text.setText("Account Update Unsuccessfully");
-                background.setCardBackgroundColor( getResources().getColor(R.color.red));
-                text.setTextColor( getResources().getColor( R.color.white ));
-                toast.setView(layout);
-                toast.show();
 
-            }else{
-                background.setCardBackgroundColor( getResources().getColor(R.color.green));
-                text.setTextColor( getResources().getColor( R.color.white ));
-                text.setText("Account Update Successfully");
-                toast.setView(layout);
-                toast.show();
-                Intent intent = new Intent( EditAccount.this , MainActivity.class );
-              intent.putExtra( "Account" , newAccount );
-                startActivity(intent);
-                finish();
+                AccountModel newAccount = new AccountModel();
+                newAccount.setId(account.getId());
+                newAccount.setAccountName(name.getText().toString().trim());
+                newAccount.setAccountType(spinner.getItemAtPosition(spinner.getSelectedItemPosition()).toString().trim());
+                newAccount.setAccountDescription(description.getText().toString().trim());
 
+                boolean result = db.updateAccount(newAccount);
+
+                //inflate layout
+                View layout = getLayoutInflater().inflate(R.layout.toast_message, (ViewGroup) view.findViewById(R.id.toastRoot));
+                TextView text = layout.findViewById(R.id.textMsg);
+                CardView background = layout.findViewById(R.id.back);
+
+                //creat toast
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.BOTTOM | Gravity.CENTER, 0, 230);
+                //set text for
+                if (result == false) {
+                    text.setText("Account Update Unsuccessfully");
+                    background.setCardBackgroundColor(getResources().getColor(R.color.red));
+                    text.setTextColor(getResources().getColor(R.color.white));
+                    toast.setView(layout);
+                    toast.show();
+
+                } else {
+                    background.setCardBackgroundColor(getResources().getColor(R.color.green));
+                    text.setTextColor(getResources().getColor(R.color.white));
+                    text.setText("Account Update Successfully");
+                    toast.setView(layout);
+                    toast.show();
+                    Intent intent = new Intent(EditAccount.this, MainActivity.class);
+                    intent.putExtra("Account", newAccount);
+                    startActivity(intent);
+                    finish();
+
+                }
             }
         }
     });
